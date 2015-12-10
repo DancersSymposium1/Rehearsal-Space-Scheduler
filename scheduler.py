0a0157b9ff285404586acb0464aff41c27aa1a9a
xhamyd <xhamyd@users.noreply.github.com>
2015-12-10 15:20:03 -0500

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,2 +1,16 @@
+import pickle
+import os
 
+TIMESHEET_FOLDERS = 'timesheets/'
 
+def load_maps():
+    piece_map = pickle.load(open(MAPS_PATH + 'piece_map.p', 'r+'))
+    dancer_map = pickle.load(open(MAPS_PATH + 'dancer_map.p', 'r+'))
+    return (piece_map, dancer_map)
+
+def run():
+    (piece_map, dancer_map) = load_maps()
+    
+    for filename in os.listdir(TIMESHEET_FOLDERS.strip('/')):
+        f = open(filename)
+run()
