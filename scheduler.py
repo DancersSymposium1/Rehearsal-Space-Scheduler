14c3e998e2652e749336f568dd2c1c1f88a651ae
xhamyd <xhamyd@users.noreply.github.com>
2015-12-20 20:18:48 -0800

    Update scheduler.py
    Added cheks of safety (anti-aliasing, added file close handlers)

--- scheduler.py
+++ scheduler.py
@@ -1,12 +1,21 @@
 import pickle
 import os
+import copy
 
 TIMESHEET_FOLDERS = 'timesheets/'
 
 def load_maps():
-    piece_map = pickle.load(open(MAPS_PATH + 'piece_map.p', 'r+'))
-    dancer_map = pickle.load(open(MAPS_PATH + 'dancer_map.p', 'r+'))
-    return (piece_map, dancer_map)
+    pm = open(MAPS_PATH + 'piece_map.p', 'r+')
+    piece_map = pickle.load(pm)
+    piece_map_copy = copy.deepcopy(piece_map)
+    pm.close()
+    
+    dm = open(MAPS_PATH + 'dancer_map.p', 'r+')
+    dancer_map = pickle.load(dm)
+    dancer_map_copy = copy.deepcopy(dancer_map)
+    dm.close()
+    
+    return (piece_map_copy, dancer_map_copy)
 
 def run():
     (piece_map, dancer_map) = load_maps()
