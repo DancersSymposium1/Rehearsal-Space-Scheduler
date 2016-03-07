36be452ea38cc8e3cb7911221c645a01d76e5e63
xhamyd <xhamyd@users.noreply.github.com>
2016-03-07 01:45:36 -0500

    Very much modified the scheduler code so that the import functions would even begin to work. All of the code and text files were copied due to the local nature of Pickle. These files are up-to-date as of Monday, March 7th, 1:48:00 AM.

--- scheduler.py
+++ scheduler.py
@@ -2,24 +2,27 @@ import pickle
 import os
 import copy
 
-TIMESHEET_FOLDERS = 'timesheets/'
+from timesheet_from_csv import *
+from Classes import *
+from audition_program import Dancer, Piece, GenderConstraint
+
+TIMESHEET_FOLDER = "timesheets/"
+MAPS_PATH = "maps/"
 
 def load_maps():
-    pm = open(MAPS_PATH + 'piece_map.p', 'r+')
-    piece_map = pickle.load(pm)
-    piece_map_copy = copy.deepcopy(piece_map)
-    pm.close()
-    
-    dm = open(MAPS_PATH + 'dancer_map.p', 'r+')
-    dancer_map = pickle.load(dm)
-    dancer_map_copy = copy.deepcopy(dancer_map)
-    dm.close()
-    
-    return (piece_map_copy, dancer_map_copy)
+    piece_map = pickle.load(open(MAPS_PATH + "piece_map.p", "rb"))    
+    dancer_map = pickle.load(open(MAPS_PATH + "dancer_map.p", "rb"))
+    return (piece_map, dancer_map)
 
 def run():
+    # make sure the Audition code is run so that these maps are created 
     (piece_map, dancer_map) = load_maps()
     
-    for filename in os.listdir(TIMESHEET_FOLDERS.strip('/')):
-        f = open(filename)
+    for filename in os.listdir(TIMESHEET_FOLDER.strip("/")):
+        f = open(TIMESHEET_FOLDER + filename)
+
+        #@TODO: everything!
+        
+        f.close()
+
 run()
