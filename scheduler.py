e0aaf4987bd24c3cbfa9813a6a6b1424e32b23c4
xhamyd <xhamyd@users.noreply.github.com>
2016-03-07 22:42:01 -0500

    changed the timesheets for one single dance, edited code to reflect this

--- scheduler.py
+++ scheduler.py
@@ -8,11 +8,18 @@ from Classes import *
 
 TIMESHEET_FOLDER = "timesheets/"
 
+def free_times(T): # returns all of the times free in the timesheet
+    freeTimes = []
+    for time in T.timeHeader:
+        for day in T.dayHeader:
+            if T.checkAvail(day, time): freeTimes.append((day, time))
+    return freeTimes
+            
 def run():
     ts = dict() # dictionary of timesheets
     for filename in os.listdir(TIMESHEET_FOLDER.strip("/")):
         T = timesheet_from_csv(TIMESHEET_FOLDER + filename)
+        print free_times(T)
         ts[T.name] = T
-
     
 run()
