a2e83c15be4bfe701bd407a750d6ba36d6ec0707
xhamyd <xhamyd@users.noreply.github.com>
2016-03-07 19:43:45 -0500

    Updated the file, see keep note to see objective of code

--- scheduler.py
+++ scheduler.py
@@ -1,28 +1,18 @@
-import pickle
+import sys
 import os
-import copy
 
+# download "Timesheet" & "Rehearsal-Space-Scheduler" repos into same directory
+sys.path.append(os.path.abspath("../Timesheet"))
 from timesheet_from_csv import *
 from Classes import *
-from audition_program import Dancer, Piece, GenderConstraint
 
 TIMESHEET_FOLDER = "timesheets/"
-MAPS_PATH = "maps/"
-
-def load_maps():
-    piece_map = pickle.load(open(MAPS_PATH + "piece_map.p", "rb"))    
-    dancer_map = pickle.load(open(MAPS_PATH + "dancer_map.p", "rb"))
-    return (piece_map, dancer_map)
 
 def run():
-    # make sure the Audition code is run so that these maps are created 
-    (piece_map, dancer_map) = load_maps()
-    
+    ts = dict() # dictionary of timesheets
     for filename in os.listdir(TIMESHEET_FOLDER.strip("/")):
-        f = open(TIMESHEET_FOLDER + filename)
+        T = timesheet_from_csv(TIMESHEET_FOLDER + filename)
+        ts[T.name] = T
 
-        #@TODO: everything!
-        
-        f.close()
-
+    
 run()
